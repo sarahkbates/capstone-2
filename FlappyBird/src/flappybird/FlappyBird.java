12e3ceefc5fa144f5ea032ce1b43679a3ff3e94c
Sarah Bates <skbates00@me.com>
2020-07-10 13:38:18 -0500

    added comments

--- FlappyBird/src/flappybird/FlappyBird.java
+++ FlappyBird/src/flappybird/FlappyBird.java
@@ -18,22 +18,23 @@ import javax.swing.Timer;
 
 public class FlappyBird implements ActionListener, MouseListener, KeyListener
 {
+    //implementing interfaces to allow mouse click/space bar functionality
 
     public static FlappyBird flappyBird;
 
-    public final int WIDTH = 1200, HEIGHT = 800;
+    public final int WIDTH = 1200, HEIGHT = 800; //JFrame size
 
     public Renderer renderer;
 
     public Rectangle bird;
 
-    public ArrayList<Rectangle> columns;
+    public ArrayList<Rectangle> columns; //continuous creation of rectangles (pipes)
 
     public int ticks, yMotion, score;
 
     public boolean gameOver, started;
 
-    public Random rand;
+    public Random rand; //random placement of pipes
 
     public FlappyBird()
     {
@@ -45,17 +46,17 @@ public class FlappyBird implements ActionListener, MouseListener, KeyListener
 
         jframe.add(renderer);
         jframe.setTitle("Flappy Bird");
-        jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //game stops running when JFrame is closed
         jframe.setSize(WIDTH, HEIGHT);
         jframe.addMouseListener(this);
         jframe.addKeyListener(this);
-        jframe.setResizable(false);
+        jframe.setResizable(false); //graphics do not change when you resize
         jframe.setVisible(true);
 
         bird = new Rectangle(WIDTH / 2 - 10, HEIGHT / 2 - 10, 20, 20);
         columns = new ArrayList<Rectangle>();
 
-        addColumn(true);
+        addColumn(true); //four columns created at a time
         addColumn(true);
         addColumn(true);
         addColumn(true);
@@ -67,15 +68,17 @@ public class FlappyBird implements ActionListener, MouseListener, KeyListener
     {
         int space = 300;
         int width = 100;
-        int height = 50 + rand.nextInt(300);
+        int height = 50 + rand.nextInt(300); //height of pipes is random, so gap between changes every time
 
         if (start)
         {
+            //bottom and top pipes rendering as game plays
             columns.add(new Rectangle(WIDTH + width + columns.size() * 300, HEIGHT - height - 120, width, height));
             columns.add(new Rectangle(WIDTH + width + (columns.size() - 1) * 300, 0, width, HEIGHT - height - space));
         }
         else
         {
+            //create columns even when game has not "started"
             columns.add(new Rectangle(columns.get(columns.size() - 1).x + 600, HEIGHT - height - 120, width, height));
             columns.add(new Rectangle(columns.get(columns.size() - 1).x, 0, width, HEIGHT - height - space));
         }
@@ -91,6 +94,7 @@ public class FlappyBird implements ActionListener, MouseListener, KeyListener
     {
         if (gameOver)
         {
+            //restart once Game Over happens
             bird = new Rectangle(WIDTH / 2 - 10, HEIGHT / 2 - 10, 20, 20);
             columns.clear();
             yMotion = 0;
@@ -106,10 +110,12 @@ public class FlappyBird implements ActionListener, MouseListener, KeyListener
 
         if (!started)
         {
+            //allows you to click to restart game
             started = true;
         }
         else if (!gameOver)
         {
+            //resets bird placement once game is no longer over
             if (yMotion > 0)
             {
                 yMotion = 0;
@@ -122,12 +128,14 @@ public class FlappyBird implements ActionListener, MouseListener, KeyListener
     @Override
     public void actionPerformed(ActionEvent e)
     {
+        //how fast bird is moving, and how much y increases with each click
         int speed = 10;
 
         ticks++;
 
         if (started)
         {
+            //columns moving with game
             for (int i = 0; i < columns.size(); i++)
             {
                 Rectangle column = columns.get(i);
@@ -159,6 +167,7 @@ public class FlappyBird implements ActionListener, MouseListener, KeyListener
 
             for (Rectangle column : columns)
             {
+                //need the column.y == 0, so that you only get 1 point for passing pipes (otherwise you get 2 points because there are technically 2 pipes your passing)
                 if (column.y == 0 && bird.x + bird.width / 2 > column.x + column.width / 2 - 10 && bird.x + bird.width / 2 < column.x + column.width / 2 + 10)
                 {
                     score++;
@@ -166,6 +175,7 @@ public class FlappyBird implements ActionListener, MouseListener, KeyListener
 
                 if (column.intersects(bird))
                 {
+                    //bird can't hit pipes or game over
                     gameOver = true;
 
                     if (bird.x <= column.x)
@@ -175,6 +185,7 @@ public class FlappyBird implements ActionListener, MouseListener, KeyListener
                     }
                     else
                     {
+                        //bird  can't go through pipe
                         if (column.y != 0)
                         {
                             bird.y = column.y - bird.height;
@@ -204,15 +215,19 @@ public class FlappyBird implements ActionListener, MouseListener, KeyListener
 
     public void repaint(Graphics g)
     {
+        //sky
         g.setColor(Color.cyan);
         g.fillRect(0, 0, WIDTH, HEIGHT);
 
+        //ground
         g.setColor(Color.orange);
         g.fillRect(0, HEIGHT - 120, WIDTH, 120);
 
+        //grass
         g.setColor(Color.green);
         g.fillRect(0, HEIGHT - 120, WIDTH, 20);
 
+        //bird
         g.setColor(Color.red);
         g.fillRect(bird.x, bird.y, bird.width, bird.height);
 
@@ -240,17 +255,20 @@ public class FlappyBird implements ActionListener, MouseListener, KeyListener
         }
     }
 
+    //calling game to run
     public static void main(String[] args)
     {
         flappyBird = new FlappyBird();
     }
 
+    //click and bird moves
     @Override
     public void mouseClicked(MouseEvent e)
     {
         jump();
     }
 
+    //hit spacebar and bird moves
     @Override
     public void keyReleased(KeyEvent e)
     {
@@ -260,6 +278,7 @@ public class FlappyBird implements ActionListener, MouseListener, KeyListener
         }
     }
 
+    //methods required to implement due to interface
     @Override
     public void mousePressed(MouseEvent e)
     {
--- FlappyBird/src/flappybird/Renderer.java
+++ FlappyBird/src/flappybird/Renderer.java
@@ -3,17 +3,18 @@ package flappybird;
 import flappybird.FlappyBird;
 
 import java.awt.Graphics;
-
 import javax.swing.JPanel;
 
 public class Renderer extends JPanel
 {
 
+    //needed this here for some reason...automatically added
     private static final long serialVersionUID = 1L;
 
     @Override
     protected void paintComponent(Graphics g)
     {
+        //calling super from JPanel
         super.paintComponent(g);
 
         FlappyBird.flappyBird.repaint(g);
